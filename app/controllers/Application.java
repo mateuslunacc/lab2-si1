a8ab5d8a67e5e48dc96411689b827a618c668616
Mateus <mateuslunacc@gmail.com>
2015-06-09 23:51:17 -0300

    É possível adicionar anuncios e visualizá-lo na página principal.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,44 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.ArrayList;
 
-import views.html.*;
+import models.Anuncio;
+import models.dao.GenericDAO;
+import play.data.Form;
+import play.db.jpa.Transactional;
+import play.mvc.Controller;
+import play.mvc.Result;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
+	public static ArrayList<Anuncio> anuncios = new ArrayList<Anuncio>();
+	private static Form<Anuncio> anuncioForm = Form.form(Anuncio.class);
+	private static final GenericDAO dao = new GenericDAO();
+	
+	@Transactional
+    public static Result index() {		
+        return ok(views.html.index.render(anuncios));
     }
-
+    
+    @Transactional
+    public static Result criarAnuncio() {
+    	Form<Anuncio> formAnuncio = Form.form(Anuncio.class);
+    	
+    	Anuncio anuncio = formAnuncio.bindFromRequest().get();
+    	
+    	anuncios.add(anuncio);
+    	
+    	return ok(views.html.index.render(anuncios));
+    }
+    
+    //TO DO
+    public static Result novoAnuncio() {
+    	Form<Anuncio> filledForm = anuncioForm.bindFromRequest();
+    	
+    	Anuncio anuncio= filledForm.get();
+    	dao.persist(anuncio);
+    	dao.flush();
+    	
+    	return redirect(routes.Application.index());
+    }
 }
